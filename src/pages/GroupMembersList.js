22155b4b8e8b440fde0070f1e25733836afc078d
andinetd <andider1996@gmail.com>
2024-03-02 23:19:26 +0300

    Initial commit

---
+++ src/pages/GroupMembersList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GroupMembersList from './GroupMembersList'; // Assuming the component is in a file named GroupMembersList.js
+
+const App = () => {
+  return (
+    <div>
+      <h1>My Group</h1>
+      <GroupMembersList />
+    </div>
+  );
+};
+
+export default App;
